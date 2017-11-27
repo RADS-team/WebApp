43d4f162e40c798fe267b2cd5bad8c4b0c516352
loup ollivier <loup-ollivier@hotmail.fr>
2017-11-27 16:46:42 +0100

    commit 27/11

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
+import { TabsPage } from './../pages/tabs/tabs';
 import { RejoinSessionPage } from './../pages/rejoin-session/rejoin-session';
 import { CreerSessionPage } from './../pages/creer-session/creer-session';
 import { Component,ViewChild } from '@angular/core';
 import { Platform, MenuController, Nav } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
-import { TabsPage } from '../pages/tabs/tabs';
 
 @Component({
   templateUrl: 'app.html'
@@ -26,7 +25,8 @@ export class MyApp {
     // set our app's pages
     this.pages = [
       { title: 'Creer Session', component: CreerSessionPage },
-      { title: 'Rejoindre Session', component: RejoinSessionPage }
+      { title: 'Rejoindre Session', component: RejoinSessionPage },
+      { title: 'Retour accueil', component: TabsPage }
     ];
   }
 
